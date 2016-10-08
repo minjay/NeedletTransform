fbfcb7fcac26c559e326479f415adc135b63e585
minjay <mjfan@ucar.edu>
2016-10-07 22:06:59 -0700

    Custom the plot function

--- plot_field_map.m
+++ plot_field_map.m
@@ -1,19 +1,19 @@
 function plot_field_map(field)
+% function to plot fields
 
-B_ref =  makerefmat('RasterSize', size(B_r), ...
+B_ref =  makerefmat('RasterSize', size(field), ...
     'Latlim', [-90 90], 'Lonlim', [-180 180]);
 
 load coast
 
 clf
-C_max = 200;
+C_max = max(abs(field(:)));
 dC = 50;
-B_tmp = B_r;
-filename_out = 'Map_Br_lithosphere_20_110_CHAOS-6.png';
+B_tmp = field;
 
 % North pole
-pole_width = .3;
-axes('Position', [0  0.45 pole_width pole_width], 'Box', 'off')
+pole_width = .35;
+axes('Position', [0  0.65 pole_width pole_width], 'Box', 'off')
 axesm('MapProjection', 'ortho', 'Origin', [90 0], 'FLatLimit',[-Inf 40.01])
 plotm(lat, long, '-k', 'Linewidth', 1)
 meshm(B_tmp, B_ref, size(B_tmp))
@@ -23,7 +23,7 @@ caxis(C_max*[-1 1])
 set(gca, 'Box', 'off', 'Visible', 'off')
 
 % South pole
-axes('Position', [1-pole_width 0.45 pole_width pole_width ], 'Box', 'off')
+axes('Position', [1-pole_width 0.65 pole_width pole_width ], 'Box', 'off')
 axesm('MapProjection', 'ortho', 'Origin', [-90 0], 'FLatLimit',[-Inf 39.9])
 plotm(lat, long, '-k', 'Linewidth', 1)
 meshm(B_tmp, B_ref, size(B_tmp))
@@ -33,7 +33,7 @@ caxis(C_max*[-1 1])
 set(gca, 'Box', 'off', 'Visible', 'off')
 
 % Hammer projection
-axes('Position', [0 0 1 .5], 'Box', 'off')
+axes('Position', [0 0 1 0.7], 'Box', 'off')
 axesm('MapProjection', 'hammer', 'Frame', 'off')
 meshm(B_tmp, B_ref, size(B_tmp))
 plotm(lat, long, '-k', 'Linewidth', 1)
@@ -47,11 +47,9 @@ tightmap
 nio_colormap;
 map=colormap;
 
-axes('Position', [0.333 0.6 .333 .015], 'Box', 'off')
+axes('Position', [0.333 0.75 .333 .015], 'Box', 'off')
 image([1:length(map)], 'XData', [-C_max C_max]);
-set(gca,'XTick', [-C_max:dC:C_max], 'XtickLabel', [-C_max:dC:C_max], 'Xdir', 'Normal', 'FontWeight', 'normal', 'Xcolor', 'k', 'FontSize', 8, 'YTickLabel', {}, 'Ydir', 'Normal')
-text(0.5, 2,' nT', 'Color', 'k', 'FontWeight','normal', 'FontSize', 10, 'Units', 'normalized', 'HorizontalAlignment', 'Center')
-set(gcf, 'PaperOrientation', 'Portrait', 'PaperType', 'A4', 'PaperUnits', ...
-    'centimeters', 'PaperPosition', [0.2 0.2 0.7*[30 30]]);
+set(gca,'XTick', [-C_max:dC:C_max], 'XtickLabel', [-C_max:dC:C_max], 'Xdir', 'Normal', 'FontWeight', 'normal', 'Xcolor', 'k', 'FontSize', 12, 'YTickLabel', {}, 'Ydir', 'Normal')
+text(0.5, 2,' nT', 'Color', 'k', 'FontWeight','normal', 'FontSize', 12, 'Units', 'normalized', 'HorizontalAlignment', 'Center')
 
 end
