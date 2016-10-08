5c40ab7d1203b160667a84107394a7bdbe918d94
minjay <mjfan@ucar.edu>
2016-10-07 17:05:18 -0700

    Add script to get needlet coefficients for B_r, B_theta and B_phi

--- get_needlet_coef.m
+++ get_needlet_coef.m
@@ -1,8 +1,22 @@
-%% Map of Radial component of Lithospheric field B_r (n>20) at Earth's surface, from CHAOS-6 field model
+% Script to apply fast needlet transform to the synthetic magnetic field.
+%
+% Dependencies:
+% 1. S2Kit_Matlab
+% 2. NeedMat
+% They are available on Minjie's github: https://github.com/minjay.
+%
+% Steps:
+% 1. Simulate the synthetic magnetic field.
+% 2. Get the spherical harmonic coefficients using the quadrature formula.
+% 3. Get the needlet coefficients using fast needlet transform.
+%
+% Assumption: the field is isotropic. Otherwise, the histogram of the 
+% needlet coefficients would be meaningless.
 
-clear all;
-close all;
+clear
+close all
 
+%% Simulate the synthetic magnetic field
 rad = pi/180;
 a = 6371.2; % Earth's mean spherical radius
 
@@ -10,13 +24,77 @@ a = 6371.2; % Earth's mean spherical radius
 load 'CHAOS-6.mat'
 N_C6 = 120; m_C6 = g; 
 
-% Setup grid
-Step = .5;
-theta = [180-Step/2:-Step:0];
-phi   = [-180+Step/2:Step:180-Step/2];
-
 % Synthesize spherical harmonic model m_C6 into grid of B_r at Earth's surface
 n_max = 120;
 rho = (a+0)/a;
+
+% Use the equiangular grid in S2Kit
+% See http://www.cs.dartmouth.edu/~geelong/sphere/s2-main.pdf
+bandwidth = n_max+1;
+index_all = 0:(2*bandwidth-1);
+theta = 180*(2*index_all+1)/4/bandwidth;
+phi = 360*index_all/2/bandwidth;
+% -180<=phi<=180
+phi(phi>180) = phi(phi>180)-360;
+
 [B_r, B_theta, B_phi] = synth_grid(m_C6(1:n_max*(n_max+2)), rho, theta, phi);
 
+B = 2;
+
+%% Work on B_r
+B_r_transpose = B_r';
+
+% Use the quadrature formula to get the spherical harmonic coefficients
+alm = spharmonic_tran_s2kit(B_r_transpose(:), bandwidth, '/Users/minjay/Documents/MATLAB/Needlets/S2Kit_Matlab');
+
+% Use fast needlet transform to get the needlet coefficients
+beta_B_r = spneedlet_tran(alm, n_max, B);
+
+% Plot histogram
+figure
+for i = 1:8
+    subplot(2, 4, i)
+    histogram(beta_B_r{i})
+    title(['j = ', num2str(i-1)])
+end
+suptitle('Histogram of needlet coefficients for B_r')
+
+%% Work on B_theta
+B_theta_transpose = B_theta';
+
+% Use the quadrature formula to get the spherical harmonic coefficients
+alm = spharmonic_tran_s2kit(B_theta_transpose(:), bandwidth, '/Users/minjay/Documents/MATLAB/Needlets/S2Kit_Matlab');
+
+% Use fast needlet transform to get the needlet coefficients
+beta_B_theta = spneedlet_tran(alm, n_max, B);
+
+% Plot histogram
+figure
+for i = 1:8
+    subplot(2, 4, i)
+    histogram(beta_B_theta{i})
+    title(['j = ', num2str(i-1)])
+end
+suptitle('Histogram of needlet coefficients for B_{\theta}')
+
+%% Work on B_phi
+B_phi_transpose = B_phi';
+
+% Use the quadrature formula to get the spherical harmonic coefficients
+alm = spharmonic_tran_s2kit(B_phi_transpose(:), bandwidth, '/Users/minjay/Documents/MATLAB/Needlets/S2Kit_Matlab');
+
+% Use fast needlet transform to get the needlet coefficients
+beta_B_phi = spneedlet_tran(alm, n_max, B);
+
+% Plot histogram
+figure
+for i = 1:8
+    subplot(2, 4, i)
+    histogram(beta_B_phi{i})
+    title(['j = ', num2str(i-1)])
+end
+suptitle('Histogram of needlet coefficients for B_{\phi}')
+
+%% Save needlet coefficients
+filename = 'beta_B.mat';
+save(filename, 'beta_B_r', 'beta_B_theta', 'beta_B_phi')
