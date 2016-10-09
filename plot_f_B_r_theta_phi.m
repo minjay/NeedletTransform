ad28bb6870d12a1003f57b878023a01874b68e75
minjay <mjfan@ucar.edu>
2016-10-09 09:50:04 -0700

    Plot f_B_theta and f_B_phi

--- plot_f_B_r_theta_phi.m
+++ plot_f_B_r_theta_phi.m
@@ -9,3 +9,23 @@ for i = 1:8
     end
     print(['f_B_r_', num2str(i), '.eps'], '-depsc')
 end
+
+for i = 1:8
+    max_value = max(abs(f_B_theta{i}(:)));
+    if max_value<=200
+        plot_field_map(f_B_theta{i})
+    else
+        plot_field_map(f_B_theta{i}, 200)
+    end
+    print(['f_B_theta_', num2str(i), '.eps'], '-depsc')
+end
+
+for i = 1:8
+    max_value = max(abs(f_B_phi{i}(:)));
+    if max_value<=200
+        plot_field_map(f_B_phi{i})
+    else
+        plot_field_map(f_B_phi{i}, 200)
+    end
+    print(['f_B_phi_', num2str(i), '.eps'], '-depsc')
+end
